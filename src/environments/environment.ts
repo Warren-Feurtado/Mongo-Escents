7dd56e5c14372a7f0b48b3c43d7f0439e70cda00
Warren Feurtado <wolfefeurtado@gmail.com>
2022-09-15 01:56:01 -0500

    server switched to production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   API_SERVER: 'http://localhost:2000',
   API_PRODUCTION_SERVER: 'https://e-scents.herokuapp.com'
 };
